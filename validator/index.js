8b7afb9546b9a34daece55e291234fd025cb01d9
David Passaro <davepassaro@yahoo.com>
2022-09-16 12:05:01 -0700

    validation added

--- validator/index.js
+++ validator/index.js
@@ -1,24 +1,25 @@
-exports.createPostValidator = (req, res, next) =>{
-    // Title
-    req.check('title', "Write a title").notEmpty()
-    req.check('title', 'Title must be between 4 to 150 characters ').isLength({
-        min: 4,
-        max: 150
-    })
-    // body
-    req.check('title', "Write a title").notEmpty()
-    req.check('title', 'Title must be between 4 to 150 characters ').isLength({
-        min: 4,
-        max: 150
-    })
-    // check for errors
-    const errors = req.validationErrors()
-    // if error show them as they appear
-    if(errors){
-        //show first error
-        const firstError = errors.map(error=>error.msg)[0]
-        return res.status(400).json({error: firstError})
-    }
-    // proceed to next middleware
-    next();
-}
+const {check, validationResult} = require('express-validator');
+
+exports.createPostValidator = [
+        // title
+        check('title', "Write a title").notEmpty(),
+        check('title', 'Title must be between 4 to 150 characters ').isLength({
+            min: 4,
+            max: 150
+        }),
+        // body
+        check('body', "Write a title").notEmpty(),
+        check('body', 'Title must be between 4 to 150 characters ').isLength({
+            min: 4,
+            max: 150
+        }),
+        (req,res) => {
+            console.log(req)
+            // check for errors
+            const errors = validationResult(req)
+            if(errors){
+                console.log(errors)
+                return res.status(400).json({errors})
+            }
+        }
+    ]
